e466d33f324cbb2390af30af84d1ed8146d3467a
tima <fatizrajaallah@gmail.com>
2023-03-16 15:46:20 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - Entry point
+ * main - A program that print a line using the printf f unction
  * Return: 0 (Success)
  */
 
@@ -9,3 +9,4 @@ int main(void)
 printf("with proper grammar, but the outcome is a piece of art,\n")
 return (0);
 }
+
