66bb6bc732f84b9d8bfe9a87938998c98e1a3354
tima <fatizrajaallah@gmail.com>
2023-03-17 16:04:52 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,12 +1,10 @@
 #include <stdlib.h>
+#include <time.h>
 #include <stdio.h>
 /**
- * main - Entry point
- * The output of the program should be:
- *   if the last digit of n >5: the string and is >5
- *   if the last digit of n is 0: the string and is 0
- *   if the last digit of n <6 and not 0: the string and is <6 and not 0
- * Return: Always 0 (Success)
+ * main - print a text
+ * Return: Always (Success)
+ *
  */
 int main(void)
 {
