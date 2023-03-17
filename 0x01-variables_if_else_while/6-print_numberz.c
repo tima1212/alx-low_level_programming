bf60a2f6c6f2f4f71f4b63dc2a9c96b0ba18e629
tima <fatizrajaallah@gmail.com>
2023-03-17 16:46:00 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - print numbers
+ *
+ * Return: Always (Success)
+ */
+int main(void)
+{
+putchar('0');
+putchar('1');
+putchar('2');
+putchar('3');
+putchar('4');
+putchar('5');
+putchar('6');
+putchar('7');
+putchar('8');
+putchar('9');
+putchar('\n');
+return (0);
+}
