7715cc7dd6e256c895cff73ab9655dad9c99f554
tima <fatizrajaallah@gmail.com>
2023-04-05 17:54:21 -0700

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
- * puts_recursion - function like puts();
- * 
+ * _puts_recursion - function like puts();
+ * @s: input
  * Return: Always 0
  */
 void _puts_recursion(char *s)
